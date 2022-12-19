e4390fbd01953c4136386451b5f11449b675f5b2
Rems6T <remi.tidda@gmail.com>
2022-12-19 12:17:06 +0100

    BO_Encheres

---
+++ ENI_Encheres/src/main/java/fr/eni/ENI_Encheres/bo/Encheres.java
@@ -0,0 +1,49 @@
+package fr.eni.ENI_Encheres.bo;
+
+import java.sql.Date;
+
+public class Encheres {
+	   private int noUtilisateur;
+	   private int noArticle;
+	   private Date dateEnchere;
+	   private int montantEnchere;
+
+	   public Encheres(int noUtilisateur, int noArticle, Date dateEnchere, int montantEnchere) {
+	      this.noUtilisateur = noUtilisateur;
+	      this.noArticle = noArticle;
+	      this.dateEnchere = dateEnchere;
+	      this.montantEnchere = montantEnchere;
+	   }
+
+	   public int getNoUtilisateur() {
+	      return noUtilisateur;
+	   }
+
+	   public void setNoUtilisateur(int noUtilisateur) {
+	      this.noUtilisateur = noUtilisateur;
+	   }
+
+	   public int getNoArticle() {
+	      return noArticle;
+	   }
+
+	   public void setNoArticle(int noArticle) {
+	      this.noArticle = noArticle;
+	   }
+
+	   public Date getDateEnchere() {
+	      return dateEnchere;
+	   }
+
+	   public void setDateEnchere(Date dateEnchere) {
+	      this.dateEnchere = dateEnchere;
+	   }
+
+	   public int getMontantEnchere() {
+	      return montantEnchere;
+	   }
+
+	   public void setMontantEnchere(int montantEnchere) {
+	      this.montantEnchere = montantEnchere;
+	   }
+	}
