d75cee6d2a67643973202e559e7c971bd8d16e7f
ENI-Amelie <121015985+ENI-Amelie@users.noreply.github.com>
2022-12-19 16:13:49 +0100

    IHM CREER/MODIFIER COMPTE
    JSP + Servlet

---
+++ ENI_Encheres/src/main/java/fr/eni/ENI_Encheres/controleurs/ModifierCompte.java
@@ -0,0 +1,66 @@
+package fr.eni.ENI_Encheres.controleurs;
+
+import java.io.IOException;
+
+import fr.eni.ENI_Encheres.bo.Utilisateur;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Modifier
+ */
+@WebServlet("/ModifierCompte")
+public class ModifierCompte extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
+	{
+		
+		int id;
+		Utilisateur user;
+		id=Integer.parseInt(request.getParameter("id"));
+		user=UtilisateurManager.getInstance().findById(id);
+		request.setAttribute("utilisateur", user);
+		getServletContext().getRequestDispatcher("/WEB-INF/ModifierCompte.jsp").forward(request, response);
+		
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
+	{ 			int id;
+	Utilisateur user;
+	id=Integer.parseInt(request.getParameter("id"));
+	user=UtilisateurManager.getInstance().findById(id);	
+		
+		if(request.getServletPath().equals("/enregistrer"))
+	{
+			user.setPseudo(request.getParameter("pseudo"));
+			user.setNom(request.getParameter("nom"));
+			user.setPrenom(request.getParameter("prenom"));
+			user.setEmail(request.getParameter("email"));
+			user.setTelephone(request.getParameter("telephone"));
+			user.setRue(request.getParameter("rue"));
+			user.setCodePostal(request.getParameter("codePostal"));
+			user.setVille(request.getParameter("ville"));
+			user.setMotDePasse(request.getParameter("motDePasse"));
+			UtilisateurManager.getInstance().modifier(user);
+			response.sendRedirect("Accueil"); }
+
+		
+		else if(request.getServletPath().equals("/supprimer")) {
+			UtilisateurManager.getInstance().supprimer(user);
+			response.sendRedirect("Accueil");
+		}
+		
+		
+	}
+		
+	
+
+{
---
+++ ENI_Encheres/src/main/webapp/WEB-INF/CreerCompte.jsp
@@ -0,0 +1,76 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<!DOCTYPE html>
+<html>
+
+<jsp:include page="/WEB-INF/fragments/head.jsp"></jsp:include>
+
+
+<body class="container">
+
+	<%@ include file="./fragments/header.html"%>
+
+	<h1>Mon profil</h1>
+
+	<form method="post" action="ajouter">
+	
+		<div class="row g-3">
+			<div class="col">
+				<label> Pseudo : </label><input type="text" name="pseudo" pattern="[a-zA-Z0-9-]"> <br>
+			</div>
+			<div class="col">
+				<label> Nom : </label><input type="text" name="nom"> <br>
+			</div>
+		</div>
+
+		<div class="row g-3">
+			<div class="col">
+				<label> Prénom : </label><input type="text" name="prenom"> <br>
+			</div>
+			<div class="col">
+				<label> Email : </label><input type="text" name="email"> <br>
+			</div> </div>
+			
+					<div class="row g-3">
+			<div class="col">
+				<label> Téléphone : </label><input type="text" name="telephone"> <br>
+			</div>
+			<div class="col">
+				<label> Rue : </label><input type="text" name="rue"> <br>
+			</div> </div>
+			
+								<div class="row g-3">
+			<div class="col">
+				<label> Code Postal : </label><input type="text" name="codePostal"> <br>
+			</div>
+			<div class="col">
+				<label> Ville : </label><input type="text" name="ville"> <br>
+			</div> </div>
+			
+			<div class="row g-3">
+			<div class="col">
+				<label> Mot de passe : </label><input type="text" name="motDePasse"> <br>
+			</div>
+			<div class="col">
+				<label> Confirmation : </label><input type="text" name="motDePasse_confirm"> <br>
+			</div> </div>
+			
+			
+			<div class="row g-3">
+			<div class="col">
+    <button type="submit" class="btn btn-primary"name="action" value="creer">Créer</button>
+			</div>
+			<div class="col">
+    <button type="submit" class="btn btn-primary"name="action" value="annuler">Annuler</button>
+			</div> </div>
+			
+	</form>		
+
+
+
+</body>
+
+<jsp:include page="/WEB-INF/fragments/footer.html"></jsp:include>
+
+</html>
