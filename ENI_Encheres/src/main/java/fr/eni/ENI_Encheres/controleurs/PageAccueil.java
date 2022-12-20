bfaa1bbbdc19aeb4c2cf58499f5221a77953cafc
Rems6T <remi.tidda@gmail.com>
2022-12-20 14:04:46 +0100

    Page accueil

---
+++ ENI_Encheres/src/main/java/fr/eni/ENI_Encheres/controleurs/PageAccueil.java
@@ -0,0 +1,22 @@
+package fr.eni.ENI_Encheres.controleurs;
+
+import java.io.IOException;
+import java.sql.Date;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/Accueil")
+public class PageAccueil extends HttpServlet {
+	
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String name = request.getParameter("name");
+		String category = request.getParameter("category");
+	}
+}
