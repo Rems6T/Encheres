d961546463e8d6c67c5404a3ec8c1ebac68a3c20
ENI-Amelie <121015985+ENI-Amelie@users.noreply.github.com>
2022-12-21 14:16:37 +0100

    bug

--- ENI_Encheres/src/main/java/fr/eni/ENI_Encheres/controleurs/Connexion.java
+++ ENI_Encheres/src/main/java/fr/eni/ENI_Encheres/controleurs/Connexion.java
@@ -1,7 +1,13 @@
 package fr.eni.ENI_Encheres.controleurs;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
+import fr.eni.ENI_Encheres.bll.BLLException;
+import fr.eni.ENI_Encheres.bll.UtilisateurManager;
+import fr.eni.ENI_Encheres.bo.Utilisateur;
+import fr.eni.ENI_Encheres.dal.DALException;
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -14,42 +20,73 @@ import jakarta.servlet.http.HttpSession;
 public class Connexion extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-		// Récupération de la session utilisateur
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/Connexion.jsp");
+        dispatcher.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/Connexion.jsp");
+		PrintWriter out= response.getWriter();
+		
 		HttpSession session = request.getSession();
+		
+		String erreur = null;
+		
+		String identifiant = request.getParameter("pseudo");
+		String password = request.getParameter("mdp");
+		
+		
+		if(identifiant.length()==0 || identifiant.isEmpty()){
+				
+			//cr�ation de l'erreur
+			request.setAttribute("erreur", "pseudo non renseigné. Veuillez le saisir...");
+			erreur = (String) session.getAttribute("erreur");
+			out.println(erreur);
+			
+			//redirection vers la page de connexion pour saisir le login
+			this.getServletContext().getRequestDispatcher("/Connexion").forward(request, response);
+			
+			
+		}else if(password.length()==0 || password.isEmpty()) {
+				
+			//cr�ation de l'erreur
+			request.setAttribute("erreur", "mot de passe non renseign�. Veuillez le saisir...");
+			erreur = (String) session.getAttribute("erreur");
+			out.println(erreur);
+			//redirection vers la page de connexion pour saisir le login
+			this.getServletContext().getRequestDispatcher("/Connexion").forward(request, response);
+			
+		}else {
+			
+			try {
+		
+				//Valider pseudo utilisateur, verification si il est bien dans la bdd
+				Utilisateur user = UtilisateurManager.selectByPseudo(identifiant);
+				//Si la connexion est reussie
+				if(user!= null && password.equals(user.getMotDePasse())) {
+					request.getSession().setAttribute("ConnectedUser", user);
+					
+					this.getServletContext().getRequestDispatcher("/Accueil").forward(request, response);
+					
+				} else {
+					request.setAttribute("erreur", "pseudo et/ou mot de passe incorrect(s)! Veuillez ressaisir vos identifiants...");
+					erreur = (String) session.getAttribute("erreur");
+					out.println(erreur);
+					this.getServletContext().getRequestDispatcher("/Connexion").forward(request, response);
+				}
+			} catch (BLLException | DALException e) {
+				request.setAttribute("erreur", e);
+				this.getServletContext().getRequestDispatcher("/ServletErreurPage").forward(request, response);
+			}
+		
+		}}}
 
-		// Vérification de l'existence d'une session utilisateur
-		if (session.getAttribute("pseudo") != null) {
-			// Si une session utilisateur existe, redirection vers la page d'accueil
-			response.sendRedirect("Accueil.jsp");
-		} else {
-			// Si aucune session utilisateur n'existe, affichage de la page de connexion
-			request.getRequestDispatcher("Connexion.jsp").forward(request, response);
-		}
-	}
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
 
-		// Récupération des données de connexion envoyées par le formulaire de connexion
-		String username = request.getParameter("pseudo");
-		String password = request.getParameter("motDePasse");
-
-		// Vérification des informations de connexion
-		if (username.equals("admin") && password.equals("admin")) {
-			// Si les informations de connexion sont correctes, création d'une session
-			// utilisateur
-			HttpSession session = request.getSession();
-			session.setAttribute("pseudo", username);
-
-			// Redirection vers la page d'accueil
-			response.sendRedirect("Accueil.jsp");
-		} else {
-			// Si les informations de connexion sont incorrectes, affichage d'un message
-			// d'erreur
-			request.setAttribute("errorMessage", "Nom d'utilisateur ou mot de passe incorrect");
-			request.getRequestDispatcher("Connexion.jsp").forward(request, response);
-		}
-	}
-}
+	
